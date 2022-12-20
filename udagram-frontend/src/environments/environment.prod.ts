4e34f4529fb7366e5d41241b102106122ef76057
Moustafa-Ashraf <moustapha_achraf8@yahoo.com>
2022-12-20 04:11:56 +0200

    public IP for frontend/reverseproxy + ScreenShots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a02585d87d5354ecf80ccb122e831bb8-1984262925.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
